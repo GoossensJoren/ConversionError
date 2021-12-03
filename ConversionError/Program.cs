9a5ac06ddfb45288635b3338f9eacfc218fbd649
GoossensJoren <joren.goossens1@telenet.be>
2021-12-03 09:19:55 +0100

    Created console app to reproduce document conversion error

---
+++ ConversionError/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using GemBox.Document;
+
+namespace ConversionError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "contract1.docx");
+            var document = DocumentModel.Load(path);
+
+            var data = new
+            {
+                first_name = "John"
+            };
+
+            document.MailMerge.Execute(data);
+
+            using var stream = new MemoryStream();
+            document.Save(stream, SaveOptions.DocxDefault);
+
+            var documentData = stream.ToArray();
+        }
+    }
+}
